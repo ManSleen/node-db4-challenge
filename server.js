1fc4a831d1140d4d8c67f513a9f047d17bc70699
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-15 18:16:16 -0600

    ingredients router and model complete

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const helmet = require("helmet");
 
 const RecipeRouter = require("./recipes/recipe-router.js");
 const IngredientsRouter = require("./ingredients/ingredients-router.js");
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
 server.use("/api/recipes", RecipeRouter);
 server.use("/api/ingredients", IngredientsRouter);
