05be9381cadf9cddc16e049496efcfdcb5f29832
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-15 18:47:06 -0600

    Adding seeds

---
+++ data/migrations/20190815181818_bootstrap.js
@@ -0,0 +1,46 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("recipes", tbl => {
+      tbl.increments();
+      tbl
+        .string("name", 255)
+        .notNullable()
+        .unique();
+      tbl.string("steps", 4000).notNullable();
+    })
+    .createTable("ingredients", tbl => {
+      tbl.increments();
+      tbl
+        .string("name", 255)
+        .notNullable()
+        .unique();
+      tbl.float("quantity").notNullable();
+      tbl.string("unit_of_measure", 255).notNullable();
+    })
+    .createTable("recipes_ingredients", tbl => {
+      tbl.increments();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("recipes_ingredients")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
+};
